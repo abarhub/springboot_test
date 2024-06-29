ea674ec71055b60d1f1b591dc5f580a0da981772
abarhub <abarhub@gmail.com>
2024-06-29 15:48:26 +0200

    création des modules

---
+++ core/src/main/java/org/testspringboot3/springboot_test/core/DateUtils.java
@@ -0,0 +1,4 @@
+package org.testspringboot3.springboot_test.core;
+
+public class DateUtils {
+}
--- webapp/src/main/java/org/testspringboot3/springboot_test/webapp/WebappApplication.java
+++ webapp/src/main/java/org/testspringboot3/springboot_test/webapp/WebappApplication.java
@@ -1,13 +1,13 @@
-package org.testspringboot3.springboot_test;
+package org.testspringboot3.springboot_test.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootTestApplication {
+public class WebappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootTestApplication.class, args);
+        SpringApplication.run(WebappApplication.class, args);
     }
 
 }
