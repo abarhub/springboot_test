4d0b8f8bd1be26b197e1a1de29e7d5798e206177
abarhub <abarhub@gmail.com>
2024-06-30 18:56:01 +0200

    nettoyage des dépendances sur l'api client et serveur

--- batch/src/main/java/org/testspringboot3/springboot_test/batch/service/ClientService.java
+++ batch/src/main/java/org/testspringboot3/springboot_test/batch/service/ClientService.java
@@ -1,6 +1,5 @@
 package org.testspringboot3.springboot_test.batch.service;
 
-import org.apache.http.HttpHost;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -8,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.testspringboot3.springboot_test.api.AccountsApi;
 import org.testspringboot3.springboot_test.invoker.ApiClient;
+import org.testspringboot3.springboot_test.model.DepositRequest;
+
+import java.math.BigDecimal;
 
 @Service
 public class ClientService {
@@ -17,7 +19,7 @@ public class ClientService {
     public void appel() {
         LOGGER.info("appel");
 
-        RestClient restClient=RestClient.builder()
+        RestClient restClient = RestClient.builder()
                 .requestFactory(new HttpComponentsClientHttpRequestFactory())
                 .baseUrl("http://localhost:8080")
                 .build();
@@ -26,6 +28,16 @@ public class ClientService {
         AccountsApi accountsApi = new AccountsApi(apiClient);
         var res = accountsApi.getAccount();
         LOGGER.info("res: {}", res);
+        var montant = res.getBalance().longValueExact();
+
+        DepositRequest depositRequest = new DepositRequest();
+        depositRequest.depositAmount(BigDecimal.valueOf(20));
+        accountsApi.depositToAccount(depositRequest);
+
+        var res2 = accountsApi.getAccount();
+        LOGGER.info("res2: {} ({})", res2,
+                (res2.getBalance().longValueExact() == montant + 20L) ? "OK" : "KO");
+
 
     }
 
