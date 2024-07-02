4e9aaec76b4214b93adbd551e80190615dfb5282
abarhub <abarhub@gmail.com>
2024-07-02 08:14:06 +0200

    test kafk avec ack manuel

--- webapp/src/main/java/org/testspringboot3/springboot_test/webapp/config/KafkaConsumerConfig.java
+++ webapp/src/main/java/org/testspringboot3/springboot_test/webapp/config/KafkaConsumerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +22,7 @@ public class KafkaConsumerConfig {
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group-id");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        configProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         return new DefaultKafkaConsumerFactory<>(configProps);
     }
 
@@ -28,7 +30,26 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+//        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
         return factory;
     }
 
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory2() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group-id");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory2() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        return factory;
+    }
 }
--- webapp/src/main/java/org/testspringboot3/springboot_test/webapp/service/MessageConsumer.java
+++ webapp/src/main/java/org/testspringboot3/springboot_test/webapp/service/MessageConsumer.java
@@ -1,8 +1,10 @@
 package org.testspringboot3.springboot_test.webapp.service;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +12,22 @@ public class MessageConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
 
-    @KafkaListener(topics = "my-topic", groupId = "my-group-id")
+    @KafkaListener(topics = "my-topic", groupId = "my-group-id",
+            containerFactory = "kafkaListenerContainerFactory")
     public void listen(String message) {
-        LOGGER.info("Received message: {}", message);
+        LOGGER.info("Received message : {}", message);
+    }
+
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group-id2",
+            containerFactory = "kafkaListenerContainerFactory2")
+    public void listen2(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) {
+        String message=record.value();
+        LOGGER.info("Received message2: {} (partition:{},offset:{})", message,record.partition(),record.offset());
+        if(false) {
+            LOGGER.info("ack");
+            acknowledgment.acknowledge();
+        }
     }
 
 
