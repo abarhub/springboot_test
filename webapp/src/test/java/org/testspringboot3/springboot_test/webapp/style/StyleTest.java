27090ab43e782cd8209fdc5148e57aa9d87a2ea3
abarhub <tulipebleu1@laposte.net>
2024-07-06 17:06:57 +0200

    ajout d'une classe de test

---
+++ webapp/src/test/java/org/testspringboot3/springboot_test/webapp/style/StyleTest.java
@@ -0,0 +1,12 @@
+package org.testspringboot3.springboot_test.webapp.style;
+
+public class StyleTest {
+
+    public void test1() {
+
+        var x = 10;
+        System.out.println(x);
+
+        var foo = new StringBuilder().append(10).append("aaaa").append("xdfdfdfggf").append(9542).toString();
+    }
+}
