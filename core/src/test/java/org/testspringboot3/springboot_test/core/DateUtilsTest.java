45c56281e3fa67d24cb21f36d1d5f5a14de622f7
abarhub <tulipebleu1@laposte.net>
2024-07-07 10:10:02 +0200

    ajout des logs

---
+++ core/src/test/java/org/testspringboot3/springboot_test/core/DateUtilsTest.java
@@ -0,0 +1,46 @@
+package org.testspringboot3.springboot_test.core;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateUtilsTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DateUtilsTest.class);
+
+    @AfterEach
+    void tearDown() {
+        MDC.clear();
+    }
+
+    @Test
+    void getDate() {
+        LOGGER.info("getDate");
+        var getDate = DateUtils.getDate();
+        assertNotNull(getDate);
+    }
+
+    @Test
+    void test2() {
+        LOGGER.info("test2");
+        MDC.put("cle1", "valeur1");
+        LOGGER.info("suite");
+    }
+
+    @Test
+    void test3() {
+        LOGGER.info("test3");
+        MDC.put("cle2", "valeur2");
+        LOGGER.atInfo().addKeyValue("test","test3").log("suite3");
+        try {
+            int i = 1 / 0;
+            LOGGER.info("i={}", i);
+        } catch (Exception e) {
+            LOGGER.error("erreur pour diviser par 0", e);
+        }
+    }
+}
